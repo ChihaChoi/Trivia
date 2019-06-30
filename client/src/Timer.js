ffd0c37abcf2751cf93a303905de500274b6917d
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-06-30 16:07:36 +0100

    fix countdown no longer counts down past 0

--- client/src/Timer.js
+++ client/src/Timer.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import socket from './socket'
+import { TLSSocket } from 'tls';
 
 
 
 class Timer extends Component {
     constructor(props) {
         super(props);
-        this.state={time: 0}
+        this.state={time: 0, maxTime: null, finishState: 0}
     }
 
     componentDidMount(){
-        socket.on('time',time=>this.setState({time:time}))        
+        socket.on('time',time=>{
+            //set max timer for countdown animation
+            if(this.state.maxTime === null){
+                this.setState({maxTime:time})
+            }
+            //prevent countdown past 0
+            if(this.state.finishState === 0){
+                if(time===0){ this.setState ({finishState:1}) }
+                this.setState({time:time})
+            }
+
+        })    
+
+            
     }
 
 
     render() { 
-        return ( <div className="timer">{this.state.time}</div> );
+        return ( <div className="timer">{this.state.time}/{this.state.maxTime}</div> );
     }
 }
  
