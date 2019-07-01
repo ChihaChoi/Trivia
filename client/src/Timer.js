27b86903bddfa56b2ed366d15cea442d58f2c5c9
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-07-01 16:47:55 +0100

    add countdown animation on timer

--- client/src/Timer.js
+++ client/src/Timer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import socket from './socket'
-import { TLSSocket } from 'tls';
-
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 
 class Timer extends Component {
@@ -11,6 +11,7 @@ class Timer extends Component {
     }
 
     componentDidMount(){
+        //timer number logic 
         socket.on('time',(time,mode)=>{
             console.log(time,"---",mode)
                 //countdown timer on waiting screen will now countdown for both q&a timers
@@ -39,12 +40,36 @@ class Timer extends Component {
                     this.setState({time:time})
                 }
             }   
-        })   
+        })
+        //============================================   
     }
 
 
     render() { 
-        return ( <div className="timer">{this.state.time}/{this.state.maxTime}</div> );
+        return ( <div className="timer">
+        <CircularProgressbar counterClockwise="true" value={this.state.time} maxValue={this.state.maxTime }text={`${this.state.time}`}
+        
+        styles={buildStyles({
+            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+            strokeLinecap: 'butt',
+         
+            // Text size
+            textSize: '16px',
+         
+            // How long animation takes to go from one percentage to another, in seconds
+            pathTransitionDuration: 1,
+         
+            // Can specify path transition in more detail, or remove it entirely
+            pathTransition: 'linear',
+         
+            // Colors
+            pathColor: `white`,
+            textColor: 'white',
+            trailColor: 'transparent',
+            backgroundColor: 'transparent',
+          })}
+          />;
+        </div> );
     }
 }
  
