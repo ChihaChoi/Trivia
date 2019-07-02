5a6196cec764edda90477ca7873de323c302de4e
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-07-02 17:13:41 +0100

    fix score screen updates on waiting screen

--- client/src/Login/WaitingScreen.js
+++ client/src/Login/WaitingScreen.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Timer from '../Timer'
 import ScoreButton from '../Trivia/ScoreButton'
+import socket from '../socket'
+
 
 class WaitingScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { playerData: "" }
+    }
+    componentDidMount(){
+        socket.on('playerData',(data)=>{
+            this.setState({playerData:data})
+            console.log('received')
+        })
     }
     render() { 
         return ( 
         <div className="notMuch">
-            <ScoreButton />
+            <ScoreButton  playerData={this.state.playerData} />
             <div className="container waitingScreen">
                 <h1 className="waiting--title">Next round starts in </h1>
                 <Timer mode="waiting"/>
