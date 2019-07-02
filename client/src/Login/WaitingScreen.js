7e67f0c2e9eb1f418713ece347c1a6baa6cc53b2
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-07-02 17:09:45 +0100

    add Score button to waiting page

--- client/src/Login/WaitingScreen.js
+++ client/src/Login/WaitingScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Timer from '../Timer'
+import ScoreButton from '../Trivia/ScoreButton'
 
 class WaitingScreen extends Component {
     constructor(props) {
@@ -8,10 +9,14 @@ class WaitingScreen extends Component {
     }
     render() { 
         return ( 
-        <div className="container waitingScreen">
-            <h1 className="waiting--title">Next round starts in </h1>
-            <Timer mode="waiting"/>
-        </div> );
+        <div className="notMuch">
+            <ScoreButton />
+            <div className="container waitingScreen">
+                <h1 className="waiting--title">Next round starts in </h1>
+                <Timer mode="waiting"/>
+            </div>
+        </div>
+         );
     }
 }
  
