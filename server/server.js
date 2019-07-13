4887f733632ac036b07c1fbc803fe90772dc4da8
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-07-13 18:08:52 +0100

    fix app.listen port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ PORT = 8081
 const express = require('express')
 const app = express()
 // app.use(express.static(__dirname + "/public"))
-const expressServer = app.listen(PORT)
+const expressServer = app.listen(process.env.PORT || PORT)
 
 const socketio = require('socket.io')
 const io = socketio(expressServer)
