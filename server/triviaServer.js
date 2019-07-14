54d0fa8ff2c56f048c38284fed5ce77d275fde9f
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-07-14 16:30:43 +0100

    fix server

--- server/triviaServer.js
+++ server/triviaServer.js
@@ -10,11 +10,13 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  server.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  server.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-   });
+  });
 
 const io = socketIO(server);
 
