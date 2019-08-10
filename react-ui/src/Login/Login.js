34f185dd76f53201fbb1f9c62ee55162f718065b
Chiha <51158851+ChihaChoi@users.noreply.github.com>
2019-08-10 16:19:30 +0100

    add subheader to title page

--- react-ui/src/Login/Login.js
+++ react-ui/src/Login/Login.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
-import LoginScreen from './LoginScreen'
-import WaitingScreen from './WaitingScreen'
-import Logo from '../transitions/Logo'
+import React, { Component } from "react";
+import LoginScreen from "./LoginScreen";
+import WaitingScreen from "./WaitingScreen";
+import Logo from "../transitions/Logo";
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    // componentDidMount(){
-    //     socket.on('question',()=>{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  // componentDidMount(){
+  //     socket.on('question',()=>{
 
-    //     })
-    // }
-    changeToWaiting(){
-        this.setState({waiting:"on"})
-        this.props.changeToWaiting()
-    }
-    render() { 
-        return ( <div>
-            <div className="login__logo">
-                <Logo />
-            </div>
-            {this.state.waiting ? 
-            <WaitingScreen /> : 
-            <LoginScreen fullScreen={this.props.fullScreen} changeToWaiting={this.changeToWaiting.bind(this)}/>
-            }
-
-        </div> );
-    }
+  //     })
+  // }
+  changeToWaiting() {
+    this.setState({ waiting: "on" });
+    this.props.changeToWaiting();
+  }
+  render() {
+    return (
+      <div>
+        <div className="login__logo">
+          <Logo />
+          <p className="login__sub-header">Chiha's Real-Time Trivia!</p>
+        </div>
+        {this.state.waiting ? (
+          <WaitingScreen />
+        ) : (
+          <LoginScreen
+            fullScreen={this.props.fullScreen}
+            changeToWaiting={this.changeToWaiting.bind(this)}
+          />
+        )}
+      </div>
+    );
+  }
 }
- 
+
 export default Login;
